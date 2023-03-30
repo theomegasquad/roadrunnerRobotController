ea7002bc9d1a36ff69d63de46755817f53cc469e
amogh <amogh.babu@gmail.com>
2023-03-29 20:05:18 -0700

    Added MeepMeep Visualization Tool and Module and its dependencies.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MotionTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MotionTest1.java
@@ -1,27 +1,34 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
+
+@Config
+@Autonomous(group = "drive")
 public class MotionTest1 extends LinearOpMode {
+
+    public static double DISTANCE = 50;
+
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                .strafeRight(10)
-                .forward(5)
+        Pose2d startPose = new Pose2d(-36, 68, Math.toRadians(270));
+        drive.setPoseEstimate(startPose);
+
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(-12, 44), Math.toRadians(315), SampleMecanumDrive.getVelocityConstraint(0.2, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
                 .build();
 
         waitForStart();
-
-        if(isStopRequested()) return;
-
-        drive.followTrajectory(myTrajectory);
+        drive.followTrajectory(traj1);
     }
 }
